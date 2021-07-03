2e16e5c9cb795f5b3c153c301d2cf128a9864b77
siddharthLama <59767129+siddharthLama@users.noreply.github.com>
2021-07-04 01:40:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import ScanScreen from './screens/Scanscreen';
+
+export default class App extends React.Component {
+  render(){
+    return (
+        <AppContainer />
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Scan: {screen: ScanScreen}
+});
+
+const AppContainer =  createAppContainer(TabNavigator);
